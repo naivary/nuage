31492753627ca764bb46105395f6e44566e520c4
Sayed Mustafa Hussaini <SayedMustafaHussaini@outlook.de>
2025-11-24 23:29:55 +0100

    int is sufficient for now

--- size.go
+++ size.go
@@ -1,14 +1,8 @@
 package nuage
 
-type DataSize int64
-
 const (
-	KiB DataSize = 1
-	MiB          = 1024 * KiB
-	GiB          = 1024 * MiB
-	TiB          = 1024 * GiB
+	KiB int = 1
+	MiB     = 1024 * KiB
+	GiB     = 1024 * MiB
+	TiB     = 1024 * GiB
 )
-
-func (d DataSize) Int() int {
-	return int(d)
-}
