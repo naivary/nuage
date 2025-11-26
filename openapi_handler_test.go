d29d5f42ec4abe856984756f5a86893367efc20c
Sayed Mustafa Hussaini <SayedMustafaHussaini@outlook.de>
2025-11-26 23:07:05 +0100

    testing the result of the openapi querier is missing yet

--- openapi_handler_test.go
+++ openapi_handler_test.go
@@ -20,20 +20,19 @@ func TestQueryOpenAPIDoc(t *testing.T) {
 			},
 		},
 	}
-
 	doc := &OpenAPI{
 		Info: &Info{
 			Version: "v1.0.0",
 			Title:   "test-title",
 		},
 	}
-
 	q, err := NewOpenAPIQuerier(doc)
 	if err != nil {
 		t.Errorf("OpenAPI Querier: %v", err)
 	}
 	hl := queryOpenAPIDoc(q)
 	r := nuagetest.NewRequest(http.MethodGet, "", nil)
+
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
 			res, err := hl(r, tc.input)
