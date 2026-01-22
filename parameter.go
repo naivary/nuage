2a1afd9cd12f204c53a32de9cfa5e5109ba83745
Sayed Mustafa Hussaini <SayedMustafaHussaini@outlook.de>
2026-01-22 23:33:47 +0000

    this branch is about the generator not the openapi

--- parameter.go
+++ parameter.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
-	"reflect"
 )
 
 var ErrParamStyleNotSupported = errors.New(`
@@ -19,17 +18,6 @@ Currently the following parameter styles are supported:
 
 If you have a concrete use case that cannot be expressed with the supported styles, please open a GitHub issue and describe the problem you are trying to solve`)
 
-var errParamTagNotFound = errors.New(
-	"parameter tag `query`, `path`, `cookie` or `header` not found",
-)
-
-const (
-	_tagKeyPathParam   = "path"
-	_tagKeyQueryParam  = "query"
-	_tagKeyCookieParam = "cookie"
-	_tagKeyHeaderParam = "header"
-)
-
 type paramTagOpts struct {
 	in           ParamIn
 	name         string
@@ -37,72 +25,6 @@ type paramTagOpts struct {
 	required     bool
 	explode      bool
 	isDeprecated bool
-}
-
-func paramType(tag reflect.StructTag) (ParamIn, error) {
-	_, ok := tag.Lookup(_tagKeyPathParam)
-	if ok {
-		return ParamInPath, nil
-	}
-	_, ok = tag.Lookup(_tagKeyQueryParam)
-	if ok {
-		return ParamInQuery, nil
-	}
-	_, ok = tag.Lookup(_tagKeyHeaderParam)
-	if ok {
-		return ParamInHeader, nil
-	}
-	_, ok = tag.Lookup(_tagKeyCookieParam)
-	if ok {
-		return ParamInCookie, nil
-	}
-	return "", errParamTagNotFound
-}
-
-func isParamExploded(fieldType reflect.Type) bool {
-	switch fieldType.Kind() {
-	case reflect.Map, reflect.Slice, reflect.Array:
-		return true
-	default:
-		return false
-	}
-}
-
-func parseParamTagOpts(field reflect.StructField) (*paramTagOpts, error) {
-	paramIn, err := paramType(field.Tag)
-	if err != nil {
-		return nil, err
-	}
-	return &paramTagOpts{
-		in: paramIn,
-        explode: isParamExploded(field.Type),
-	}, nil
-}
-
-func newOpenAPIParamFor[T any]() ([]*Parameter, error) {
-	rtype := reflect.TypeFor[T]()
-	fields := reflect.VisibleFields(rtype)
-	for _, field := range fields {
-		if field.Anonymous {
-			continue
-		}
-		opts, err := parseParamTagOpts(field)
-		if err != nil {
-			return nil, err
-		}
-		var param *Parameter
-		var paramErr error
-
-		switch opts.in {
-		case ParamInPath:
-			param, paramErr = newPathParam(opts)
-		}
-		if paramErr != nil {
-			return nil, err
-		}
-
-	}
-	return nil, nil
 }
 
 func newPathParam(opts *paramTagOpts) (*Parameter, error) {
