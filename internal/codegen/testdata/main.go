8fb27ed6857e3871ca7e8e449d48f2656c541929
Sayed Mustafa Hussaini <SayedMustafaHussaini@outlook.de>
2026-01-29 16:07:49 +0000

    the case of ptr->named->ptr is not generated correctly rn

--- internal/codegen/testdata/main.go
+++ internal/codegen/testdata/main.go
@@ -1,6 +1,13 @@
 package main
 
+type (
+	NamedInt int
+	PtrNamed *int
+)
+
 type PathParamRequest struct {
-	Int    int  `path:"integer"`
-	PtrInt *int `path:"ptr_integer"`
+	Int      int       `path:"integer"`
+	PtrInt   *int      `path:"ptr_integer"`
+	Named    NamedInt  `path:"named"`
+	PtrNamed *PtrNamed `path:"ptr_named"`
 }
