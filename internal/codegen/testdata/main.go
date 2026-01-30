fceb00cd2086b751d83112cf754f822a2b73a992
Sayed Mustafa Hussaini <SayedMustafaHussaini@outlook.de>
2026-01-30 08:51:26 +0000

    string has to be assigned to the name of the parameter defined in the struct not the general val

--- internal/codegen/testdata/main.go
+++ internal/codegen/testdata/main.go
@@ -3,6 +3,7 @@ package main
 type (
 	NamedInt int
 	PtrNamed *int
+	String   string
 )
 
 type PathParamRequest struct {
@@ -10,4 +11,6 @@ type PathParamRequest struct {
 	PtrInt   *int      `path:"ptr_integer"`
 	Named    NamedInt  `path:"named"`
 	PtrNamed *PtrNamed `path:"ptr_named"`
+	Str      string    `path:"str"`
+	StrNamed String    `path:"str_named"`
 }
